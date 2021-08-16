8f2605d6caa2ecf8d88cbe04aba3ebbc14a01d61
Trevor Pirie <email@example.com>
2021-08-17 06:11:40 +1200

    added welcome and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: WelcomeComponent, pathMatch: 'full' },
+  { path: '**', component: WelcomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, WelcomeComponent, AboutComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
